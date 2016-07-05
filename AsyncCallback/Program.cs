ea125a38c5193a5c7e253b2bdaf2b33abdbd5fb7
lawphotog <laurence.nyein@gmail.com>
2016-07-05 10:49:24 +0100

    init

---
+++ AsyncCallback/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncCallback
+{
+    class Program
+    {
+        static byte[] buffer = new byte[100];
+
+        static void Main(string[] args)
+        {
+            //TestCallbackAPM();
+            int i = 0;
+
+            while (i <= 10000)
+            {
+                Console.WriteLine(i);
+                i += 1;
+            }
+
+            Console.ReadLine();
+        }
+
+        static void TestCallbackAPM()
+        {
+            string filename = System.IO.Path.Combine(System.Environment.CurrentDirectory, "ConnectArchitecture.pdf");
+
+            FileStream strm = new FileStream(filename,
+                FileMode.Open, FileAccess.Read, FileShare.Read, 1024,
+                FileOptions.Asynchronous);
+
+            // Make the asynchronous call
+            IAsyncResult result = strm.BeginRead(buffer, 0, buffer.Length,
+                new System.AsyncCallback(CompleteRead), strm);
+        }
+
+        static void CompleteRead(IAsyncResult result)
+        {
+            Console.WriteLine("Read Completed");
+
+            FileStream strm = (FileStream)result.AsyncState;
+
+            // Finished, so we can call EndRead and it will return without blocking
+            int numBytes = strm.EndRead(result);
+
+            // Don't forget to close the stream
+            strm.Close();
+
+            Console.WriteLine("Read {0} Bytes", numBytes);
+            Console.WriteLine(BitConverter.ToString(buffer));
+        }
+    }
+}
---
+++ Yield/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list1 = new List<object> { new object[1], new object[2] };
+            var list2 = new List<object> { new object[3], new object[4] };
+
+            var prog = new Program();
+            var name = prog.EfficientMerge(list1, list2);
+
+            foreach (var o in name)
+            {
+                Console.WriteLine(o.GetType());
+            }
+
+            Console.ReadLine();
+        }
+
+        public IEnumerable<object> EfficientMerge(List<object> list1, List<object> list2)
+        {
+            foreach (var o in list1)
+                yield return o;
+            foreach (var o in list2)
+                yield return o;
+        }
+    }
+}
