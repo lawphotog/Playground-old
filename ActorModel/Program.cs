4f514eb8dd91fae5efc0b016c49231031ea666a9
Laurence Aung <laurence.nyein@gmail.com>
2016-09-23 14:52:02 +0100

    x

---
+++ ActorModel/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Akka;
+using Akka.Actor;
+
+namespace ActorModel
+{
+    public class Greet
+    {
+        public Greet(string who)
+        {
+            Who = who;
+        }
+
+        public string Who { get; private set; }
+    }
+
+    public class GreetingActor : ReceiveActor
+    {
+        public GreetingActor()
+        {
+            Receive<Greet>(greet =>
+               Console.WriteLine("Hello {0}", greet.Who));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new actor system (a container for your actors)
+            var system = ActorSystem.Create("MySystem");
+
+            // Create your actor and get a reference to it.
+            // This will be an "ActorRef", which is not a
+            // reference to the actual actor instance
+            // but rather a client or proxy to it.
+            var greeter = system.ActorOf<GreetingActor>("greeter");
+
+            // Send a message to the actor
+            greeter.Tell(new Greet("World"));
+
+            // This prevents the app from exiting
+            // before the async work is done
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Yield/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            var list1 = new List<Fruit> { new Fruit { Id = 1, Name = "Apple" }, new Fruit { Id = 2, Name = "Orange" } };
+            var list2 = new List<Fruit> { new Fruit { Id = 3, Name = "Pear" }, new Fruit { Id = 4, Name = "Strewberry" } };
+
+            var prog = new Program();
+            var names = prog.EfficientMerge(list1, list2);
+
+            foreach (var o in names)
+            {
+                Console.WriteLine(o.Name);
+            }
+        
+            Console.ReadLine();
+        }
+
+        public IEnumerable<Fruit> EfficientMerge(List<Fruit> list1, List<Fruit> list2)
+        {
+            foreach (var o in list1)
+                yield return o;
+            foreach (var o in list2)
+                yield return o;
+        }
+
+        public class Fruit
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
