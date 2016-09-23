ee263e99ae865cddb669a50a2815fe0d8678deeb
Laurence Aung <laurence.nyein@gmail.com>
2016-09-23 15:31:12 +0100

    x

--- Mongo/Program.cs
+++ Mongo/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Mongo
 {
@@ -11,8 +13,22 @@ namespace Mongo
         static void Main(string[] args)
         {
             //persist fruits
+            var client = new MongoClient("mongodb://localhost/?w=1");
 
+            var database = client.GetDatabase("test");
 
+            var collection = database.GetCollection<BsonDocument>("fruits");
+
+            var document = new BsonDocument
+            {
+                { "Name", "apple" }
+            };
+
+            collection.InsertOne(document);
+
+            var objectId = document["_id"].ToString();
+
+            
         }
 
         public IEnumerable<Fruit> Fruits { get; set; }
