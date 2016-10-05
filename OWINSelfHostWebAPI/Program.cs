55ebb3c28ea4fc32f5982f0bffe1a97054cc33d4
Laurence Aung <laurence.nyein@gmail.com>
2016-10-05 12:57:26 +0100

    added OWINSelfHostWebAPI

---
+++ OWINSelfHostWebAPI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using Microsoft.Owin.Hosting;
+
+namespace WebApplication2
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+                var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                if (response != null)
+                {
+                    Console.WriteLine("Information from service: {0}", response.Content.ReadAsStringAsync().Result);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: Impossible to connect to service");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Press ENTER to stop the server and close app...");
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OWINSelfHostWebAPI/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Owin;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
