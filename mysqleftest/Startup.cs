a70069a7658a507f684cc6accff1f52dfe86779c
lawphotog <laurence.nyein@gmail.com>
2016-10-09 00:27:54 +0100

    added projects

---
+++ mysqleftest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mysqleftest.Startup))]
+namespace mysqleftest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
