d3702c79156a75e55c41f675f80bdcef77893d7a
Laurence Aung <laurence.nyein@gmail.com>
2017-01-05 12:35:50 +0000

    casting Inheritance example

---
+++ InheritanceCasting/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var honda = new Honda()
+            {
+                HondaSpecialFeature = "Honda drives fast"
+            };
+
+            var aCar = (Car)honda;
+
+            if (aCar.GetType() == typeof(Honda))
+            {
+                var hondaCar = (Honda)aCar;
+
+                Console.WriteLine("this car is a Honda");
+                Console.WriteLine(hondaCar.HondaSpecialFeature);
+            }
+
+            Console.Read();
+        }
+
+        class Car
+        {
+            public Car()
+            {
+                Name = "Honda";
+            }
+
+            public Car(string model) : this()
+            {
+                Model = model;
+            }
+
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string Model { get; set; }
+
+        }
+
+        class Honda : Car
+        {
+            public string HondaSpecialFeature { get; set; }
+        }
+    }
+}
