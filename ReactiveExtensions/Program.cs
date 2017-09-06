ece7ca5c1978ad6dc35ecc162798a78618de1a0d
Laurence Aung <laurence.nyein@gmail.com>
2017-09-06 18:24:00 +0100

    update

--- ReactiveExtensions/Program.cs
+++ ReactiveExtensions/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Reactive.Linq;
+
 
 namespace ReactiveExtensions
 {
@@ -11,13 +12,7 @@ namespace ReactiveExtensions
     {
         static void Main(string[] args)
         {
-            var query = from number in Enumerable.Range(1, 5) select number;
-
-            foreach (var number in query)
-            {
-                Console.WriteLine(number);
-            }
-            //Console.ReadLine();
+            var query = from number in Enumerable.Range(1, 5000) select number;
 
             var obserableQuery = query.ToObservable();
             obserableQuery.Subscribe(Console.WriteLine);
